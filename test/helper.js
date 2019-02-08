6f52ad327586783e4da68dc58ec0e21278f5e58c
Russell Koons <rkoons@gmail.com>
2019-02-08 15:47:39 -0600

    updated travis

--- test/helper.js
+++ test/helper.js
@@ -1,13 +1,12 @@
 'use strict';
 const dotenv = require('dotenv');
-const knex = require('../knex')
+
 dotenv.config();
 
 const {runServer, closeServer} = require('../server');
 
 before(function() {
-  knex.migrate.latest()
-    .then(() =>  runServer(process.env.TEST_PORT));
+  return runServer(process.env.TEST_PORT)
 });
 
 after(function() {
