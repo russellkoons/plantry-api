633626e5b50148bd3e0b45461cb0e6e123bc0018
Russell Koons <rkoons@gmail.com>
2019-02-10 09:20:37 -0600

    nothing feels good

--- knexfile.js
+++ knexfile.js
@@ -16,8 +16,8 @@ module.exports = {
     client: 'pg',
     connection: {
       database: process.env.TEST_DATABASE_NAME,
-      user: process.env.TEST_USERNAME,
-      password: process.env.TEST_PASSWORD
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD
     },
     pool: {
       min: 2,
