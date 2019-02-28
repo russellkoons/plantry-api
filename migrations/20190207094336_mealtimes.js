03c3c96e1eae8e1682d1d7a51a1a9c2446cef75a
Russell Koons <rkoons@gmail.com>
2019-02-28 12:43:03 -0600

    fixed a migration

--- migrations/20190207094336_mealtimes.js
+++ migrations/20190207094336_mealtimes.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('mealtimes', function(t) {
       t.increments('id').primary();
-      t.integer('meal_id').references('meals.id');
-      t.integer('time_id').references('times.id');
+      t.integer('meal_id').references('meals.id').onDelete('CASCADE');
+      t.integer('time_id').references('times.id').onDelete('CASCADE');
     })
   ]);
 };
